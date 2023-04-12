c68b828009ba87db30e427624afa1db15575b8d9
mecduda99 <rm98760@fiap.com.br>
2023-04-11 22:29:46 -0300

    "criando uma tabuada com while"

--- enquanto.py
+++ enquanto.py
@@ -1,6 +1,14 @@
 x=0
 
-while x < 10:
-  print(x)
-  # x = x + 1
-  x+=1
+# for i in range(0,10):
+#     while x < 10:
+#     print(x)
+#     if x == 5:
+#         break
+#     # continue = continua até ser falso
+#     # x = x + 1
+#     x+=1
+
+while True:
+    print(x)
+    x+=1
---
+++ tabuada.py
@@ -0,0 +1,10 @@
+tabuada = 1
+n2 = 1
+
+while tabuada <= 10:
+    while n2 <=10:
+        multiplicacao = tabuada * n2
+        print(str(tabuada) + " X " + str(n2) + " = " + str(multiplicacao))
+        n2 += 1
+    tabuada += 1
+    n2 = 1
