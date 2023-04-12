c747dde2a9c4f48c76f154d4dd7af5f18ecd5688
mecduda99 <rm98760@fiap.com.br>
2023-04-11 22:35:16 -0300

    "Tabuada com for"

--- tabuada.py
+++ tabuada.py
@@ -1,10 +1,17 @@
 tabuada = 1
 n2 = 1
 
+# while tabuada <= 10:
+#     while n2 <=10:
+#         multiplicacao = tabuada * n2
+#         print(str(tabuada) + " X " + str(n2) + " = " + str(multiplicacao))
+#         n2 += 1
+#     tabuada += 1
+#     n2 = 1
 while tabuada <= 10:
-    while n2 <=10:
+    for n2 in range(1,11):
         multiplicacao = tabuada * n2
         print(str(tabuada) + " X " + str(n2) + " = " + str(multiplicacao))
         n2 += 1
     tabuada += 1
-    n2 = 1
+    
