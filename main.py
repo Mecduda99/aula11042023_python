188b8f4d48b1a46567af2d0b335cd53692c9d6dd
mecduda99 <rm98760@fiap.com.br>
2023-04-11 22:09:37 -0300

    "criando main, teste e enquanto.py"

---
+++ main.py
@@ -0,0 +1,13 @@
+lista = ["Abacate", "Morango"]
+# __atr__ = 
+# __str__ => print
+# __eq__ ==
+
+if "Abacate" in lista:
+    print()
+
+for item in lista:
+    print(item)
+
+for index in range(0, len(lista)):
+    print(lista[index])
---
+++ teste.py
@@ -0,0 +1,6 @@
+#Lazy function 1
+teste = list(range(0,10))
+
+print(type(teste))
+# print(teste)
+print(id(teste))
