1367b65b50c89ed705a0fe4d10f8cb04652b3a59
Mecduda99 <rm98760@fiap.com.br>
2023-04-13 20:52:29 -0300

    "Adicionado a tarefa de casa finalizada no repositorio"

--- while_tarefadecasa.py
+++ while_tarefadecasa.py
@@ -1,9 +1,25 @@
-# criando um while de despertador 
+# Pomodoro timer 'fake'
+contador = 0
+timer = int(input('quanto tempo você deseja para estudar?'))
+intervalo = timer / 2
 
-hora = 6
-despertador = 6.5
-silenciar = 0
+while contador <= timer:
+  if contador == 0:
+    print("iniciando contagem...")
 
-while hora <= 8:
-    print("Acordar")
+  if contador == intervalo:
+    print("Parabéns, você completou o primeiro tempo!")
+    print("Faça uma pausa de 5 minutos.")
+    continuar = input("Você deseja continuar? S/N ")
+    if continuar == "N":
+      print("contador finalizado no tempo:" + str(contador) + ":00")
+      break
+    if continuar == "S":
+      print("Ótimo! Vamos lá.")
+  print("Tempo: " + str(contador) + ":00")
+
+  if contador == timer:
+    print("Parabéns!! Você finalizou o seu tempo de estudos diário.")
+  contador += 1
+
     
